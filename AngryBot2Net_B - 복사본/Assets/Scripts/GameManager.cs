ee3dbc81cee01cafc8e2c6af821903c28fddaf5c
qwg12 <a_124@naver.com>
2023-11-27 13:34:01 +0900

    13w
    멈춤

--- AngryBot2Net_B - 복사본/Assets/Scripts/GameManager.cs
+++ AngryBot2Net_B - 복사본/Assets/Scripts/GameManager.cs
@@ -2,11 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
-public class GameManager : MonoBehaviour
+using Photon.Realtime;
+using TMPro;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class GameManager : MonoBehaviourPunCallbacks
 {
+    public TMP_Text roomName;
+    public TMP_Text connectInfo;
+    public TMP_Text msgList;
+    public Button exitBtn;
     void Awake()
     {
         CreatePlayer();
+        // 접속 정보 추출 및 표시
+        SetRoomInfo();
+        // Exit 버튼 이벤트 연결
+        exitBtn.onClick.AddListener(() => OnExitClick());
     }
     void CreatePlayer()
     {
@@ -21,4 +33,37 @@ public class GameManager : MonoBehaviour
         0);
 
     }
+
+    // 룸 접속 정보를 출력
+    void SetRoomInfo()
+    {
+        Room room = PhotonNetwork.CurrentRoom;
+        roomName.text = room.Name;
+        connectInfo.text = $"({room.PlayerCount}/{room.MaxPlayers})";
+    }
+    // Exit 버튼의 OnClick에 연결할 함수
+    private void OnExitClick()
+    {
+        PhotonNetwork.LeaveRoom();
+    }
+    // 포톤 룸에서 퇴장했을 때 호출되는 콜백함수
+    public override void OnLeftRoom()
+    {
+        SceneManager.LoadScene("Lobby");
+    }
+    // 룸으로 새로운 네트워크 유저가 입장했때 호출되는 콜백함수 
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        SetRoomInfo();
+
+        string msg = $"\n<color=#00ff00>{newPlayer.NickName}</color> is joined room"; msgList.text += msg;
+    }
+    // 룸에서 네트워크 유저가 퇴장했때 호출되는 콜백함수 
+    public override void OnPlayerLeftRoom(Player otherPlayer)
+    {
+        SetRoomInfo();
+        string msg = $"\n<color=#ff0000>{otherPlayer.NickName}</color> is left room"; msgList.text += msg;
+    }
+
+
 }
